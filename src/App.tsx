8a4d4500df33f0c512fa16f434d1ca2e136e6aba
Maris Feagin <mfeagin64@gmail.com>
2025-03-18 17:29:31 -0400

    index formatted

--- src/App.tsx
+++ src/App.tsx
@@ -7,28 +7,45 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+          <div>
+              <a href="https://vite.dev" target="_blank">
+                  <img src={viteLogo} className="logo" alt="Vite logo"/>
+              </a>
+              <a href="https://react.dev" target="_blank">
+                  <img src={reactLogo} className="logo react" alt="React logo"/>
+              </a>
+          </div>
+          <h1>Vite + React</h1>
+          <div className="card">
+              <button onClick={() => setCount((count) => count + 1)}>
+                  count is {count}
+              </button>
+              <p>
+                  Edit <code>src/App.tsx</code> and save to test HMR
+              </p>
+          </div>
+          <p className="read-the-docs">
+              Click on the Vite and React logos to learn more
+          </p>
+          //---------------------------------------------------------------------------------------//
+
+          <div className="header">
+              <h1><em>To Go Nutrition</em></h1><img src="assets/" height="60px" width="60px" alt="green apple speeding"/>
+          </div>
+
+          <div className="login"></div>
+
+          <div className="search"></div>
+
+          <div id="map"></div>
+
+          <div className="menu"></div>
+
+          <div className="user-profile"></div>
+
+          <div className="footer"></div>
+      </>
   )
 }
 
